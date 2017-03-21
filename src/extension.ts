f21f5aa384bca17008ba5872c0eee6b2d28f36ba
MuMuShi <hnaynlin19@163.com>
2017-03-21 19:59:01 +0800

    增加cssimg 功能

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,14 @@
 'use strict';
 import * as vscode from 'vscode';
-import {tpl} from './tpl';
+import {tpl, cssImg} from './tpl';
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "vscode-tpl" is now active!');
     let disposable = vscode.commands.registerCommand('linz.tpl', tpl);
+    let css = vscode.commands.registerCommand('linz.cssImg', cssImg);
 
     context.subscriptions.push(disposable);
+    context.subscriptions.push(css);
 }
 
 // this method is called when your extension is deactivated
--- src/tpl.ts
+++ src/tpl.ts
@@ -1,5 +1,9 @@
 import * as vscode from 'vscode';
 const _ = require('lodash');
+const globby = require('globby');
+const path = require('path');
+const fs = require('fs');
+const Jimp = require('jimp');
 
 export function tpl () {
     let editor = vscode.window.activeTextEditor;
@@ -37,4 +41,69 @@ function json2hbs (data , key = '', parentKey = '') {
         html += `<p>{{${parentKey ? parentKey + '.' + key : key}}}</p>\n`;
     }
     return html;
+}
+
+
+function pathJoin(str) {
+    return path.join(vscode.workspace.rootPath, str);
+}
+
+function renderTpl(data) {
+    return `width: ${data.width}px;
+    height: ${data.height}px;
+    background-image:url(${data.src});`
+}
+
+/**
+ *  cssImg 快速插入css 图片
+ * 
+ * @export
+ */
+export function cssImg() {
+    let tmpString: string = '';
+    globby([pathJoin('/src/**/*.{png,jpg}'), pathJoin('/assets/**/*.{png,jpg}'), pathJoin('/public/**/*.{png,jpg}') , pathJoin('/images/**/*.{png,jpg}')]).then((paths) => {
+        console.log(paths);
+        let tmp = [];
+        paths.forEach((v) => {
+            tmp.push({
+                "label": path.parse(v).base,
+                "description": path.relative(vscode.workspace.rootPath, v),
+                "src":v
+            });
+        });
+        vscode.window.showQuickPick(tmp).then(function (action) {
+            console.log(action);
+            if (!action) {
+                // vscode.window.showErrorMessage('出错了1，请重试');
+                return;
+            }
+            
+            Jimp.read(action.src).then(function (image) {
+                var edit = vscode.window.activeTextEditor;
+                var imgRelativePath = path.relative(path.parse(edit.document.uri.path).dir.substring(1), action.src);
+                imgRelativePath = imgRelativePath.replace(/\\/g,'/');
+                if (/^\\/.test(imgRelativePath)) {
+                    imgRelativePath = '.' + imgRelativePath;
+                } else if (/^[a-zA-Z0-9]/.test(imgRelativePath)){
+                    imgRelativePath = './'  + imgRelativePath;
+                }
+                let imgData = {
+                    width: image.bitmap.width,
+                    height: image.bitmap.height,
+                    src: imgRelativePath
+                }
+                
+                edit.edit((editBuilder) => {
+                    var position = new vscode.Position(edit.selection.active.line, edit.selection.active.character);
+                    editBuilder.insert(position, renderTpl(imgData));
+                });
+            }).catch(function (err) {
+                console.log(err);
+                vscode.window.showErrorMessage('出错了，请重试');
+                // handle an exception
+            });
+
+        })
+        // fs.writeFileSync(pathJoin('/sass/_base64.scss'),tmpString);
+    })
 }
